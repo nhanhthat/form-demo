e8cf496d94158f71e88f070e68c6779d476fb1d4
nhanhthat <84766117+nhanhthat@users.noreply.github.com>
2023-09-09 00:05:12 +0700

    upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+function App() {
+
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:8080/get-users') 
+      .then (res => {
+        setUsers(res.data)
+      })
+  }, [])
+
+
+  const handleSubmit = () => {
+    const name = document.querySelector('.txt-name').value
+    const email = document.querySelector('.txt-email').value
+    
+    axios.post('http://localhost:8080/create', {name : name, email : email})
+      .then (res => {
+        if (res.data.status == 200) {
+          window.location.reload()
+        }
+      })
+  }
+
+
+
+  return (
+    <div className="App">
+      <div>
+        <input type='text' className='txt-name' placeholder='name'/>
+        <input type='text' className='txt-email' placeholder='name'/>
+        <button onClick={() => handleSubmit()}>Submit</button>
+      </div>
+      <div>
+        <ul>
+          {users.map((user , index) => (
+            <li key={index}>Name : {user.name} , Email : {user.email}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
